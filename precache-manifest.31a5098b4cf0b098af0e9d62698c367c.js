5e2d714afcf87d70193333e3eb7b3a99682b40bf
Nancy <lexyfeer@hotmail.com>
2019-08-05 18:26:54 -0500

    Updates

---
+++ precache-manifest.31a5098b4cf0b098af0e9d62698c367c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a809c13330f41bc83df759b9107416",
+    "url": "/rampa-finder/index.html"
+  },
+  {
+    "revision": "dc42d0ac928366e305d6",
+    "url": "/rampa-finder/static/css/main.7e68f7f4.chunk.css"
+  },
+  {
+    "revision": "d28d4538b0db9545c5a8",
+    "url": "/rampa-finder/static/js/2.e098601e.chunk.js"
+  },
+  {
+    "revision": "dc42d0ac928366e305d6",
+    "url": "/rampa-finder/static/js/main.650fc48a.chunk.js"
+  },
+  {
+    "revision": "3bef9383005004ecb22a",
+    "url": "/rampa-finder/static/js/runtime~main.04591969.js"
+  },
+  {
+    "revision": "ab3fe23b6cf7b8629a4860ce07475588",
+    "url": "/rampa-finder/static/media/rampa_logo.ab3fe23b.svg"
+  }
+]);
