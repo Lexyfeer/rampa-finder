a3bbe2a65e7d82117e8f510dd81e14fd3e94139d
unknown <myrnamares90@gmail.com>
2019-08-05 15:56:39 -0500

    se creo el componente Startups el cual contiene la conexión a la API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Link} from 'react-router-dom';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from "./components/main";
+import Startup from './components/Startup';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    render(){
+        return(
+            <div>
+               <h1>App</h1>
+               <Startup />
+            </div>
+        )
+    }
 }
 
 export default App;
