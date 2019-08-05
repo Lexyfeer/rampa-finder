8765a7c5704dc57d0f44040bfaf17db921edd4c4
Nancy <lexyfeer@hotmail.com>
2019-08-05 18:52:34 -0500

    Merge con Myrna Fetch a la api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
-import {Link} from 'react-router-dom';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import Main from "./components/main";
-import Startup from './components/Startup';
+// import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+// import Main from "./components/main";
 
 import NavbarProfile from './components/Navbar';
 
 class App extends Component{
     render(){
         return(
-            <div>
+            <div className="App">
                <NavbarProfile />
-               <h1>App</h1>
-               <Startup />
             </div>
         )
     }
--- src/components/Startups.js
+++ src/components/Startups.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 
-class Startups extends Component {
-    render() {
-            return(
-                <div>
-                    <p>Componente para mostrar todas las Startups</p>
-                    
-                    
-                </div>
-            )
+
+
+ class Startups extends Component {
+    constructor (props){
+        super(props);
+        this.state ={
+            data: []
         }
     }
+componentWillMount(){
+    fetch('http://45.232.252.23/laboratoria/public/_/items/startups', { method: 'GET',
+    headers: {
+        Authorization: 'Bearer laboratoriaToken2019',    
+        'Content-type': 'application/json; '     
+    }
+    }
+    )
+    .then(response =>  response.json ()) 
+    .then(data => this.setState({data}));
+}
+
+
+
+render () {
+    return(
+        <div> Aqui van los datos </div> 
+
+
+
+
+    )
+}
+}
 
 export default Startups;
