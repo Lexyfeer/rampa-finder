d062f2c184af03c6466c8491650ec7d67bd48457
Nancy <lexyfeer@hotmail.com>
2019-08-06 15:43:36 -0500

    Merge con Myrna funcion update

--- src/components/Startups.js
+++ src/components/Startups.js
@@ -127,7 +127,7 @@ componentWillMount(){
                         }
                     })}
                 </div>
-            </Fragment>
+            
             
                 <div>
                     <Search />
