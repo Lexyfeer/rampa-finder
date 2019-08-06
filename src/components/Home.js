c31d2961a4a0ce15a0a273ff2c106b752f326e6e
KarenHippitaku <hippitaku@gmail.com>
2019-08-06 13:25:29 -0500

    Component fixed and properly merged

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,9 @@
-constructor(props) {
+import React, { Component, Fragment } from 'react';
+import Search from './Search';
+import Chart from './Charts';
+
+class Home extends Component {
+  constructor(props) {
     super(props);
     this.state = {
         startups: [],
@@ -6,8 +11,25 @@ constructor(props) {
         updates: [],
         // chartData: {}
     }
+    this.viewStartups= this.viewStartups.bind(this);
 }
+
+viewStartups(){
+    fetch('http://45.232.252.23/laboratoria/public/_/items/startups',
+    { method: 'GET',
+        headers: {
+        Authorization: 'Bearer laboratoriaToken2019',
+        'Content-type': 'application/json; '
+    }
+    }
+    )
+    .then(response =>  response.json ())
+    .then(data => {this.setState({startups: data.data})});
+}
+
 componentWillMount() {
+  this.viewStartups();
+
     fetch('http://45.232.252.23/laboratoria/public/_/items/startups',
         {
             method: 'GET',
@@ -84,41 +106,42 @@ render() {
     const { updates } = this.state;
     return (
 
-      <div>
       <Fragment>
       <div className="contentStartups">
-      {startups.map((startup, index) => {
-        if (startup.startup_status  === 'Seguimiento') {
+        <Search />
+        <h1>Startups pendientes por evaluar</h1>
+        {startups.map((startup, index) => {
+          if (startup.startup_status  === 'Seguimiento') {
 
-          return (
-            <div className="card" key={index}>
-            <img
-            className="imgProjects card-img-top"
-            src={startup.logo}
-            alt={startup.name}
-            title={startup.name}
-            />
-            <div className="card-body">
-            <h5 className="card-title">{startup.name}</h5>
-            <p className="card-text">{startup.one_liner}</p>
-            <p className="card-text">{startup.website}</p>
-            <p className="card-text">{startup.startup_status}</p>
-            <a
-            href={startup.linkProject}
-            rel="noopener noreferrer"
-            target="_blank"
-            >
-            Ir al proyecto
-            </a>
-            <a
-            href={startup.linkReadme}
-            rel="noopener noreferrer"
-            target="_blank"
-            >
-            Ver más detalles del proyecto
-            </a>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
+            return (
+              <div className="card" key={index}>
+                <img
+                className="imgProjects card-img-top"
+                src={startup.logo}
+                alt={startup.name}
+                title={startup.name}
+                />
+              <div className="card-body">
+                <h5 className="card-title">{startup.name}</h5>
+                <p className="card-text">{startup.one_liner}</p>
+                <p className="card-text">{startup.website}</p>
+                <p className="card-text">{startup.startup_status}</p>
+                <a
+                href={startup.linkProject}
+                rel="noopener noreferrer"
+                target="_blank"
+                >
+                Ir al proyecto
+                </a>
+                <a
+                href={startup.linkReadme}
+                rel="noopener noreferrer"
+                target="_blank"
+                >
+                Ver más detalles del proyecto
+                </a>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
             </div>
           )
         }
@@ -160,7 +183,8 @@ render() {
                        )
                    }
                })}
-
-      </Fragment>
-
-      </div>
+         </Fragment>
+       )
+     }
+}
+export default Home;
